2097f75ac70cba053207a0d96233eecbf77da0c7
Mathéo Delessert <hello@matheodelssert.ch>
2025-04-07 21:04:25 +0200

    adding border radius to SearchBar.tsx

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -5,14 +5,20 @@ import colors from '@/constants/colors'
 export default function SearchBar() {
     return (
         <View style={{
-            flex: 1,
+            borderRadius: 20,
             width: 400,
-            height: 120,
+            height: 160,
             flexDirection: "column",
             backgroundColor: colors.background,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.3,
+            shadowRadius: 4,
+            elevation: 5,
+            paddingLeft: 20,
+            paddingRight: 20,
 
         }}>
-            <input type="text"/>
         </View>
     )
 }

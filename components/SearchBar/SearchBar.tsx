0e7ae10f3ce876f768d552f1577bec17b2a2b8f4
Mathéo Delessert <hello@matheodelssert.ch>
2025-04-08 09:19:52 +0200

    searchbar done but defintely need to update the uI and the color

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,24 +1,31 @@
 import { View } from 'react-native';
 import colors from '@/constants/colors'
+import PositionLine from './PositionLine'
+import TextInputSearch from './TextInputSearch'
+
 
 
 export default function SearchBar() {
     return (
         <View style={{
+            marginTop: 10,
             borderRadius: 20,
             width: 400,
-            height: 160,
-            flexDirection: "column",
+            height: 140,
+            flexDirection: "row",
+            paddingLeft: 60,
+            paddingRight: 20,
+            justifyContent: 'center',
             backgroundColor: colors.background,
             shadowColor: "#000",
             shadowOffset: { width: 0, height: 2 },
             shadowOpacity: 0.3,
             shadowRadius: 4,
             elevation: 5,
-            paddingLeft: 20,
-            paddingRight: 20,
-
         }}>
+
+            <PositionLine></PositionLine>
+            <TextInputSearch></TextInputSearch>
         </View>
     )
 }
---
+++ components/SearchBar/SearchBarLine.tsx
@@ -0,0 +1,17 @@
+import { View } from 'react-native';
+import colors from '../../constants/colors';
+
+export default function SearchBarLine () {
+    return (
+        <View style={{
+            width: 300,
+            height: 1,
+            backgroundColor: colors.SoftAnthracite,
+            borderRadius: 5,
+            opacity: 90,
+            margin: 10,
+        }}>
+
+        </View>
+    )
+}

ed3df65eda38197a2d44680cbdc9451482033207
Mathéo Delessert <hello@matheodelssert.ch>
2025-04-24 21:16:07 +0300

    updating UI of search bar by adding pretty circle

--- components/SearchBar/PositionLine.tsx
+++ components/SearchBar/PositionLine.tsx
@@ -1,5 +1,6 @@
 import { View,  } from 'react-native'
 import colors from '@/constants/colors'
+import { Svg, Circle } from 'react-native-svg'
 
 
 export default function PositionLine(){
@@ -11,17 +12,17 @@ export default function PositionLine(){
 
 
         }}>
-            <View  id='point1' style={{
-                width: 15,
-                height: 15,
-                backgroundColor: 'blue',
-                borderRadius: 10,
-            }}>
 
-            </View>
+                <Svg width="17" height="17" viewBox="0 0 17 17" fill="none">
+                    <Circle cx="8.60156" cy="8.42969" r="7.5" stroke="#008CFF"/>
+                    <Circle cx="8.60153" cy="8.4299" r="4.1926" stroke="#008CFF" stroke-width="0.75"/>
+                </Svg>
+
+
+
             <View style={{
                 height: 50,
-                width: 4,
+                width: 2,
 
                 backgroundColor: colors.SoftAnthracite,
 
@@ -29,24 +30,15 @@ export default function PositionLine(){
             >
 
             </View>
-            <View id='point2'
-                  style={{
-                width: 15,
-                height: 15,
-                backgroundColor: 'blue',
-                borderRadius: 10,
-            }}>
+
+                <Svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <Circle cx="8" cy="8" r="7.5" stroke="black"/>
+                </Svg>
 
 
-            </View>
         </View>
 
     )
 }
 
-// var p1 = point1.position;
-// var p2 = point2.position;
 
-//function SizeLine (){
-//    p1
-// }

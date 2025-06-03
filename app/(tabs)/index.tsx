8ce8448d9e4714ed666c77b8675995e92c5f20e0
Mathéo Delessert <hello@matheodelssert.ch>
2025-06-03 17:25:15 +0200

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,5 @@
 import { View } from 'react-native';
 import SearchBar from '../components/travel/SearchBar';
-import RegularIcon from '../components/travel/TrainDiv/TrainModularIcon/RegularIcon';
-import ItalicIcon from "@/app/components/travel/TrainDiv/TrainModularIcon/ItalicIcon";
-import EVIcon from "@/app/components/travel/TrainDiv/TrainModularIcon/EVIcon";
 
 export default function Travel() {
     return (
@@ -16,6 +13,7 @@ export default function Travel() {
             paddingBottom: 80, // Add padding for the menu at bottom
         }}
         >
+            <SearchBar></SearchBar>
         </View>
     );
 }
